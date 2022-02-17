18e88a180e3544927371102eb9fe73784c3678c6
unknown <patrickviegas4@gmail.com>
2022-02-16 21:45:45 -0300

    Up project :rocket:

---
+++ README.md
@@ -0,0 +1,32 @@
+# 💿 API Simples de lista de Hoteis (Alteração no Primeiro projeto do curso da udemy "REST APIs com Python e Flask, conexão com Banco de Dados")
+
+
+## 📚 Sobre o projeto
+
+<p> Esse é uma modificação do primeiro projeto <a href="https://github.com/patrickvrodrigues/Hotel-API-Flask-Projeto01-Udemy">API Simples de lista de Hoteis</a>, onde é modificado para conexão com banco de dados e adicionado try e exceptions.</p>
+
+<hr/>
+
+## 💻 Tecnologias
+
+<p>O projeto continua utilizando Python, Flask, porém, é utilizado a biblioteca "sql_alchemy" para realizar a criação, conexão e demais modificações no banco de dados. O banco de dados utilizado é em SQLite.</p>
+
+<hr />
+
+## ⚡ Funcionamento
+
+<p>Como ja dito, os dados são locais. Além disso, está implementado o get, post, put e delete, ou seja, o CRUD. Para executar o projeto, basta ter instalado a biblioteca do flask e do flask restful. Após isso, só executar o comando "python app.py".</p>
+
+
+<hr/>
+
+
+## 🔗 Links úteis
+
+<a href="https://flask.palletsprojects.com/en/2.0.x/">https://flask.palletsprojects.com/en/2.0.x/</a>
+
+<a href="https://flask-restful.readthedocs.io/en/latest/">https://flask-restful.readthedocs.io/en/latest/</a>
+
+<a href="https://www.udemy.com/course/rest-apis-com-python-e-flask/">https://www.udemy.com/course/rest-apis-com-python-e-flask/</a>
+
+<a href="https://flask-sqlalchemy.palletsprojects.com/en/2.x/">https://flask-sqlalchemy.palletsprojects.com/en/2.x/</a>
---
+++ resources/hotel.py
@@ -0,0 +1,60 @@
+from flask_restful import Resource, reqparse
+from models.hotel import HotelModel
+
+
+class Hoteis(Resource):
+    def get(self):
+        return {'hoteis': [hotel.json() for hotel in HotelModel.query.all()]}
+
+class Hotel(Resource):
+
+    argumentos = reqparse.RequestParser()
+    argumentos.add_argument('nome', type=str, required=True, 
+        help="The field 'nome' cannot be left blank")
+    argumentos.add_argument('estrelas', type=float, required=True, 
+        help="The field 'estrelelas' cannot be left blank")
+    argumentos.add_argument('diaria')
+    argumentos.add_argument('cidade')
+
+
+    def get(self, hotel_id):
+        hotel = HotelModel.find_hotel(hotel_id)
+        if hotel:
+            return hotel.json()
+        return {'message': 'Hotel not found.'}, 404
+    
+    def post(self, hotel_id):
+        if HotelModel.find_hotel(hotel_id):
+            return {"message" : "Hotel id '{}' already exists.".format(hotel_id)}, 400
+        dados = Hotel.argumentos.parse_args()
+        hotel = HotelModel(hotel_id,**dados)
+        try:
+            hotel.save_hotel()
+        except:
+            return {'message': 'An internal error ocurred trying to save hotel.'}, 500
+        return hotel.json()
+
+    def put(self, hotel_id):   
+        dados = Hotel.argumentos.parse_args()
+        hotel_encontrado = HotelModel.find_hotel(hotel_id)
+        if hotel_encontrado:
+            hotel_encontrado.update_hotel(**dados)
+            hotel_encontrado.save_hotel()
+            return hotel_encontrado.json(), 200
+        hotel = HotelModel(hotel_id,**dados)
+        try:
+            hotel.save_hotel()
+        except:
+            return {'message': 'An internal error ocurred trying to save hotel.'}, 500
+        return hotel.json(), 201
+
+    def delete(self, hotel_id):
+        hotel = HotelModel.find_hotel(hotel_id)
+        if hotel:
+            try: 
+                hotel.delete_hotel()
+                return {'message' : 'Hotel deleted.'}
+            except: 
+                {'message': 'An internal error ocurred trying to delete hotel.'}, 500
+        else:
+            return {'message' : 'Hotel not found.'}, 404
